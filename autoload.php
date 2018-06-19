7c93f1a059fdcb4291be45cb02b4c05324633b6c
Suleiman Mamman A <suleimamman@gmail.com>
2018-06-19 08:59:51 +0100

    Autoload file Edited

--- autoload.php
+++ autoload.php
@@ -1,24 +1,5 @@
 <?php
 
-// spl_autoload_register(function($class) {
-//   $prefix = 'app\\';
-  
-//   $length = strlen($prefix);
-  
-//   $base_directory = __DIR__ . '/app/';
-  
-//   if(strncmp($prefix, $class, $length) !== 0) {
-//     return;
-//   }
-  
-//   $relative_class = substr($class, $length);
-  
-//   $file = $base_directory . str_replace('\\', '/', $relative_class) . '.php';
-  
-//   if(file_exists($file)) {
-//     require $file;
-//   }
-// });
 class Autoloader {
     static public function loader($className) {
         $filename = "app/model/" . str_replace("\\", '/', $className) . ".php";
