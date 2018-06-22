a47b3528ecbc59d95e781deb755d69feff854a68
Suleiman Mamman A <suleimamman@gmail.com>
2018-06-22 05:06:55 +0100

    Check user authentication logic modify

--- app/model/check_authenticationlog.php
+++ app/model/check_authenticationlog.php
@@ -11,13 +11,14 @@ $Email_address = $_SESSION['userlog_EmailElement@NaviGaTion'];
 $userpsw = $_SESSION['userlog@cralerpsw@NaviGaTion'];
 
 $querycheck= mysqli_query($connect,"SELECT * FROM users WHERE Email='$Email_address' AND password ='$userpsw' ");
+$checkAuth = mysqli_num_rows($querycheck);
     while($revnt = mysqli_fetch_assoc($querycheck)){
       $U_NavID = $revnt['id'];
       $getFullName = $revnt['Name'];
  }
  //check if SESSION has expaired
- if(empty($Email_address) || empty($userpsw) || empty($U_NavID)){
- 	//echo "<script type='text/javascript'>window.location.href = 'http://localhost/suleiman/shop/';</script>"; 
+ if($checkAuth < 1){
+ 	echo "<script type='text/javascript'>window.location.href = 'http://localhost/suleiman/shop/';</script>"; 
 exit();
 }else{
  $_SESSION['userIdentificationNavi'] = $U_NavID; 
